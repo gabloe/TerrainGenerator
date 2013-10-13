a5244a7c6c85417e0c9f691226ba2afc8ddcd051
paranoidgabe <gloewen@crimson.ua.edu>
2013-10-13 15:36:19 -0500

    Error in test fixed.

--- tests/convertData.cpp
+++ tests/convertData.cpp
@@ -5,29 +5,31 @@
 using namespace std;
 
 int main(void) {
+   int numVertices = 10;
+   int numFields = 4;
    cout << "Converting 1d arrays to equivalent 2d arrays!\n\n";
-   float** data = (float**)malloc(10*4*sizeof(float*));
-   for (int i=0;i<10;i++) {
-      data[i] = (float*)calloc(2, sizeof(float));
-      for (int j=0;j<4;j++) {
+   float** data = (float**)malloc(numVertices * numFields * sizeof(float*));
+   for (int i=0;i<numVertices;++i) {
+      data[i] = (float*)calloc(numFields, sizeof(float));
+      for (int j=0;j<numFields;++j) {
          data[i][j] = j;
       }
    }
-   float* oneDimData = toOneDim(10,4,data);
+   float* oneDimData = toOneDim(numVertices,numFields,data);
    cout << "Sample one dimensional array of points\n";
-   for (int i=0;i<10*4;++i) {
+   for (int i=0;i<numVertices * numFields;++i) {
       cout << oneDimData[i] << " ";
    }
    cout << "\n\nSample two dimensional array of same points\n";
-   float** twoDimData = toTwoDim(10,4,oneDimData);
-   for (int i=0;i<10;++i) {
-      for (int j=0;j<4;++j) {
+   float** twoDimData = toTwoDim(numVertices, numFields, oneDimData);
+   for (int i=0;i<numVertices;++i) {
+      for (int j=0;j<numFields;++j) {
          cout << twoDimData[i][j] << " ";
       }
       cout << "\n";
    }
    free(oneDimData);
-   for (int j=0;j<10;++j) {
+   for (int j=0;j<numVertices;++j) {
       free(data[j]);
       free(twoDimData[j]);
    }
