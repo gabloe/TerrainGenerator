bfe9167d646cd5101724948bca9d44e30426a023
Jeffrey Robinson <jarobinson3@crimson.ua.edu>
2014-08-31 15:40:03 -0500

    Mean weight normal computation

--- renderer/RenderObject.cpp
+++ renderer/RenderObject.cpp
@@ -3,20 +3,10 @@
 #include <string>
 #include <cmath>
 
-bool close(Vec3 &a, Vec3 &b){
-	float x = a.getX() - b.getX();
-	float y = a.getY() - b.getY();
-	float z = a.getZ() - b.getZ();
-
-	return abs(x) < 0.0001 && abs(y) < 0.0001 && abs(z) < 0.0001;
-
-}
-Vec3 helper(Vec3 &a, Vec3 &b, Vec3 &c) {
-	Vec3 t1 = b - a;
-	Vec3 t2 = c - a;
-
-	Vec3 N = t1.cross(t2);
-
+Vec3 meanWeightEqual(const Vec3 &p1, const Vec3 &p2, const Vec3 &p3) {
+	Vec3 L1 = p2 - p1;
+	Vec3 L2 = p3 - p1;
+	Vec3 N = L1.cross(L2);
 	N.normalize();
 	return N;
 }
@@ -42,27 +32,19 @@ RenderObject::RenderObject(Shader &shader, GLfloat* vertices, int number_vertice
 		Vec3 p2(vertices + i2);
 		Vec3 p3(vertices + i3);
 
-		Vec3 t1 = helper(p1, p2, p3);
-		//Vec3 t2 = helper(p2, p3, p1);
-		//Vec3 t3 = helper(p3, p1, p2);
+		Vec3 N = meanWeightEqual(p1, p2, p3);
+		
+		normals[i1 + 0] += N.getX();
+		normals[i1 + 1] += N.getY();
+		normals[i1 + 2] += N.getZ();
 
-		/*
-		std::cout << t1 << std::endl;
-		std::cout << t2 << std::endl;
-		std::cout << t3 << std::endl;
-		*/
+		normals[i2 + 0] += N.getX();
+		normals[i2 + 1] += N.getY();
+		normals[i2 + 2] += N.getZ();
 
-		normals[i1 + 0] += t1.getX();
-		normals[i1 + 1] += t1.getY();
-		normals[i1 + 2] += t1.getZ();
-
-		normals[i2 + 0] += t1.getX();
-		normals[i2 + 1] += t1.getY();
-		normals[i2 + 2] += t1.getZ();
-
-		normals[i3 + 0] += t1.getX();
-		normals[i3 + 1] += t1.getY();
-		normals[i3 + 2] += t1.getZ();
+		normals[i3 + 0] += N.getX();
+		normals[i3 + 1] += N.getY();
+		normals[i3 + 2] += N.getZ();
 
 	}
 
