d39f7983fceba0f566ac7269a26103a868402b40
Gabriel Loewen <gabloe@microsoft.com>
2022-09-16 09:57:32 -0700

    Add material class

--- src/Resources/Mesh.cpp
+++ src/Resources/Mesh.cpp
@@ -34,25 +34,7 @@ void Mesh::Load(const aiScene* scene,
 
   int print = 0;
 
-  const aiMaterial* mtl = scene->mMaterials[mesh->mMaterialIndex];
 
-  aiColor4D material_diffuse_color = aiColor4D(1.0f, 1.0f, 1.0f, 1.0f);
-  if (AI_SUCCESS !=
-          aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &material_diffuse_color) &&
-      mesh->HasVertexColors(0) && mesh->mColors[0]) {
-    material_diffuse_color = *mesh->mColors[0];
-
-    logging::Logger::LogDebug(
-        "Mesh does not have a diffuse color, using default: Red=" +
-        std::to_string(material_diffuse_color.r) +
-        " Green=" + std::to_string(material_diffuse_color.g) +
-        " Blue=" + std::to_string(material_diffuse_color.b));
-  } else {
-    logging::Logger::LogDebug(
-        "Mesh has a diffuse color: Red=" + std::to_string(material_diffuse_color.r) +
-        " Green=" + std::to_string(material_diffuse_color.g) +
-        " Blue=" + std::to_string(material_diffuse_color.b));
-  }
   for (unsigned int i = 0; i < num_vertices; ++i) {
     double per_done = (100 * i) / num_vertices;
 
