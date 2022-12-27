db11323577a10bb18cee5be4088e3a5aff22421e
Gabriel Loewen <gabloe@microsoft.com>
2022-12-27 10:34:49 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
 
-mkdir build
-cd build
-cmake ..
-make all
+echo -e
+
+BUILD () {
+    mkdir build
+    pushd build
+    cmake ..
+    make all
+    popd
+}
+
+CLEAN () {
+    if [ -d ./build ]
+    then
+        echo "Deleting build directory"
+        rm -r build
+    else
+        echo "Nothing to do"
+    fi
+}
+
+RUN () {
+    if [ ! -d ./build ]
+    then
+        BUILD
+    fi
+    ./build/terrain-generator
+}
+
+CMD="$1"
+if [ -z "$CMD" ]
+then
+    CMD="build"
+fi
+
+echo "Command: $CMD"
+
+if [ $CMD = "build" ]
+then
+    BUILD
+fi
+
+if [ $CMD = "clean" ]
+then
+    CLEAN
+fi
+
+if [ $CMD = "run" ]
+then
+    RUN
+fi
